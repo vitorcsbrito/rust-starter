8bf7ff77ba67f5c1e7d48c3a95477f1a7483dace
Vitor Brito <vitor.brito1@specsavers.com>
2024-07-01 11:02:38 +0100

    use dynamic responder in create post req

--- starter-rest/src/routes/post_requests.rs
+++ starter-rest/src/routes/post_requests.rs
@@ -1,14 +1,18 @@
+use rocket::http::Status;
 use rocket::serde::json::Json;
 use starter_api::post::{CreatePost, PostObj};
-use starter_api::responses::custom_responder::CreatedResponder;
+use starter_api::responses::custom_responder::{DynamicStatusResponder};
 
 #[post("/", format = "json", data = "<post>")]
-pub fn create_post(post: Json<CreatePost>) -> CreatedResponder<Json<PostObj>> {
+pub fn create_post(post: Json<CreatePost>) -> DynamicStatusResponder<Json<PostObj>> {
     println!("Creating post...");
 
     let post = starter_service::create_post(post.into_inner());
-
-    CreatedResponder {
-        inner: Json(post)
+    
+    DynamicStatusResponder {
+        inner: (
+            Status::Created,
+            Json(post),
+        ),
     }
 }
